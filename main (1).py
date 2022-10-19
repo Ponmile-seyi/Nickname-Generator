ec406adf9447c9ed257be29cd4529df47cbb1af5
Oluwaponmile <105791010+Ponmile-seyi@users.noreply.github.com>
2022-10-19 21:20:13 +0100

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,9 @@
+print ('Welcome to nick-name generator!')
+
+City = input('Which City do you grow up in?\n')
+
+Pet= input('What is the name of your pet?\n')
+
+Nick_name = (City + '-' + Pet)
+
+print ('Your nick-name would be' + ' ' + Nick_name)
